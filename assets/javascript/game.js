39d356d3cff47e78cc305ac8d7f3cea321e16732
Scott Allan <sallan306@gmail.com>
2019-01-16 12:44:41 -0600

    moved game code from html to .js file

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -6,7 +6,41 @@ var pointsToWin = 0,
     redNumber = 0,
     greenNumber = 0,
     mysteryNumber =0;
+$(".gameWindow").append(
 
+
+    `
+    <div class="gameWindow" style="width: 800px; height: 700px; background: red;">
+        <div class="buttonBox" stlye="width:100%; height:20%;">
+            <button class="startButton" style="width: 25%; height: 80px;">New Game</button>
+            <button class="resetButton" style="width: 25%; height: 80px;">Reset</button>
+        </div>
+        <div class="scoreBox"       style="width:100%; height:10%;">
+            <div class="wins"       style="width: 25%">Wins: 0</div>
+            <div class="losses"     style="width: 25%">Losses: 0</div>
+        </div>
+        <div class="pointsBox" style="width:100%; height:10%;">
+                <div class="borderBox"  style="width: 25%"></div>
+                <div class="points" style="width: 25%">Current Points: 0</div>
+                <div class="pointsToWin" style="width: 25%">Points To Win: ?</div>
+                <div class="borderBox"  style="width: 25%"></div>
+            </div>
+        <div class="crystalBox" style="width:100%; height:20%;">
+            <img src="assets/images/blue.png" style="width: 24%; height: 100%;">
+            <img src="assets/images/red.png" style="width: 24%; height: 100%;">
+            <img src="assets/images/green.png" style="width: 24%; height: 100%;">
+            <img src="assets/images/mystery.png" style="width: 24%; height: 100%;">
+            <br>
+            <button class="blueButton" style="width: 24%; height: 50%;">Blue</button>
+            <button class="redButton" style="width: 24%; height: 50%;">Red</button>
+            <button class="greenButton" style="width: 24%; height: 50%;">Green</button>
+            <button class="mysteryButton" style="width: 24%; height: 50%;">Mystery</button>
+        </div>
+        `
+
+
+
+)
 
 
 $(".startButton").on("click", function(){
