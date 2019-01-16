a492e35b5b9cdf5965b44a854beea4b190197449
Scott Allan <sallan306@gmail.com>
2019-01-16 13:20:25 -0600

    finalized the UI, might make changes later

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,3 +1,5 @@
+//-----------------------------------------VARIABLE DECLARATIONS-------------------------------------------------------//
+
 var pointsToWin = 0,
     currentPoints = 0,
     wins = 0,
@@ -6,75 +8,32 @@ var pointsToWin = 0,
     redNumber = 0,
     greenNumber = 0,
     mysteryNumber =0;
+
+//-----------------------------------------RENDER GAME ELEMENTS-------------------------------------------------------//
 $(".gameWindow").append(
-
-
     `
-    <div class="gameWindow" style="width: 800px; height: 700px; background: red;">
-        <div class="buttonBox" stlye="width:100%; height:20%;">
-            <button class="startButton" style="width: 25%; height: 80px;">New Game</button>
-            <button class="resetButton" style="width: 25%; height: 80px;">Reset</button>
+        <div class="buttonBox">
+            <button class="startButton">New Game</button>
+            <button class="resetButton">Reset</button>
         </div>
-        <div class="scoreBox"       style="width:100%; height:10%;">
-            <div class="wins"       style="width: 25%">Wins: 0</div>
-            <div class="losses"     style="width: 25%">Losses: 0</div>
+        <div class="scoreBox">
+            <div class="wins">Wins: 0</div>
+            <div class="losses">Losses: 0</div>
         </div>
-        <div class="pointsBox" style="width:100%; height:10%;">
-                <div class="borderBox"  style="width: 25%"></div>
-                <div class="points" style="width: 25%">Current Points: 0</div>
-                <div class="pointsToWin" style="width: 25%">Points To Win: ?</div>
-                <div class="borderBox"  style="width: 25%"></div>
+        <div class="pointsBox">
+                <div class="points">Current Points: 0</div>
+                <div class="pointsToWin">Points To Win: ?</div>
             </div>
-        <div class="crystalBox" style="width:100%; height:20%;">
-            <img src="assets/images/blue.png" style="width: 24%; height: 100%;">
-            <img src="assets/images/red.png" style="width: 24%; height: 100%;">
-            <img src="assets/images/green.png" style="width: 24%; height: 100%;">
-            <img src="assets/images/mystery.png" style="width: 24%; height: 100%;">
-            <br>
-            <button class="blueButton" style="width: 24%; height: 50%;">Blue</button>
-            <button class="redButton" style="width: 24%; height: 50%;">Red</button>
-            <button class="greenButton" style="width: 24%; height: 50%;">Green</button>
-            <button class="mysteryButton" style="width: 24%; height: 50%;">Mystery</button>
+        <div class="crystalBox">
+            <img class="crystal" id="blueCrystal" src="assets/images/blue.png">
+            <img class="crystal" id="redCrystal" src="assets/images/red.png">
+            <img class="crystal" id="greenCrystal" src="assets/images/green.png">
+            <img class="crystal" id="mysteryCrystal" src="assets/images/mystery.png">
         </div>
         `
-
-
-
 )
 
-
-$(".startButton").on("click", function(){
-    resetGame()
-})
-$(".resetButton").on("click", function(){
-    resetGame()
-    wins = 0;
-    losses = 0;
-    $(".losses").html("Losses: " + losses)
-    $(".wins").html("Wins: " + wins)
-})
-$(".blueButton").on("click", function() {
-    currentPoints += blueNumber
-    $(".points").html("Current Points: " + currentPoints)
-    compareNumbers()
-})
-$(".redButton").on("click", function() {
-    currentPoints += redNumber
-    $(".points").html("Current Points: " + currentPoints)
-    compareNumbers()
-})
-$(".greenButton").on("click", function() {
-    currentPoints += greenNumber
-    $(".points").html("Current Points: " + currentPoints)
-    compareNumbers()
-})
-$(".mysteryButton").on("click", function() {
-    currentPoints += mysteryNumber
-    $(".points").html("Current Points: " + currentPoints)
-    compareNumbers()
-
-})
-
+//-----------------------------------------FUNCTIONS-------------------------------------------------------//
 
 function resetGame() {
     pointsToWin = getRandomInt(30, 100)
@@ -107,3 +66,38 @@ function compareNumbers() {
 
     }
 }
+//-----------------------------------------CLICK EVENTS-------------------------------------------------------//
+
+$(".startButton").on("click", function(){
+    resetGame()
+})
+$(".resetButton").on("click", function(){
+    resetGame()
+    wins = 0;
+    losses = 0;
+    $(".losses").html("Losses: " + losses)
+    $(".wins").html("Wins: " + wins)
+})
+
+$("#blueCrystal").on("click", function() {
+    currentPoints += blueNumber
+    $(".points").html("Current Points: " + currentPoints)
+    compareNumbers()
+})
+
+$("#redCrystal").on("click", function() {
+    currentPoints += redNumber
+    $(".points").html("Current Points: " + currentPoints)
+    compareNumbers()
+})
+$("#greenCrystal").on("click", function() {
+    currentPoints += greenNumber
+    $(".points").html("Current Points: " + currentPoints)
+    compareNumbers()
+})
+$("#mysteryCrystal").on("click", function() {
+    currentPoints += mysteryNumber
+    $(".points").html("Current Points: " + currentPoints)
+    compareNumbers()
+
+})
